b72b62c534d2891c59f19a927ccc9fc1c7715aae
Zed Muir <steve.g.muir@icloud.com>
2021-03-10 17:23:33 +0000

    Removed reCAPTCHA; added a honey-pot

--- sent.php
+++ sent.php
@@ -12,29 +12,7 @@
     foreach($_REQUEST['contact'] as $value) {
       $contact .= "$value, ";
 }
-
-    $recaptcha = $_POST['g-recaptcha-response'];
-    $res = reCaptcha($recaptcha);
-    if(!$res['success']){
-      // Error
-    }
-
-    function reCaptcha($recaptcha){
-      $secret = "6LcssHkaAAAAAPjW8rQ6Uhplr35CzhxIu-eQqAZ0";
-      $ip = $_SERVER['REMOTE_ADDR'];
-
-      $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-      $url = "https://www.google.com/recaptcha/api/siteverify";
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-      $data = curl_exec($ch);
-      curl_close($ch);
-
-      return json_decode($data, true);
-    }
+    if(!empty($_REQUEST['website'])) die();
 
     // Send Message
     mail( "john.greydells@gmail.com", "Contact From Website",
